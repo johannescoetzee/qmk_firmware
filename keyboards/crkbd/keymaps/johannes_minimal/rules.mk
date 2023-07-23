e72758f447537339ba231b3ca9e11e588876c9ea
Johannes Coetzee <jcoetzee@protonmail.com>
2023-07-23 13:56:38 +0200

    Remove unused corne layouts

--- keyboards/crkbd/keymaps/johannes_minimal/rules.mk
+++
@@ -1,9 +0,0 @@
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306      # Enables the use of OLED displays
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
-CONVERT_TO=promicro_rp2040
-
-EXTRAKEY_ENABLE = yes
-# TAP_DANCE_ENABLE = yes
-CAPS_WORD_ENABLE = yes
-KEY_OVERRIDE_ENABLE = yes
