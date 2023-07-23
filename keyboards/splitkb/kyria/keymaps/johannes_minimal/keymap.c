2345be8ea94a9eefeb9299a3296e126e7c07b690
Johannes Coetzee <jcoetzee@protonmail.com>
2023-07-23 13:55:09 +0200

    Fix GDM and GDN layouts

--- keyboards/splitkb/kyria/keymaps/johannes_minimal/keymap.c
+++ keyboards/splitkb/kyria/keymaps/johannes_minimal/keymap.c
@@ -77,17 +77,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                  _______, MO(GDM), KC_SPC, MO(GDN), KC_TAB ,      _______, KC_LALT, _______, _______, _______
     ),
 
-    [GDN] = LAYOUT(
-      KC_ESC , KC_SCLN, KC_COMM, KC_DOT , KC_P   , KC_Y   ,                                          KC_F   , KC_G   , KC_C   , KC_R   , KC_L   , KC_GRV ,
-      KC_LSFT, KC_A   , KC_O   , KC_E   , KC_U   , KC_I   ,                                          KC_D   , KC_H   , KC_T   , KC_N   , KC_S   , KC_RSFT,
-      KC_LCTL, KC_QUOT, KC_Q   , KC_J   , KC_K   , KC_X   , KC_B   , KC_M   ,      _______, _______, KC_B   , KC_M   , KC_W   , KC_V   , KC_Z   , KC_RCTL,
+    [GDM] = LAYOUT(
+      KC_ESC , KC_L   , KC_R   , KC_C   , KC_G   , KC_F   ,                                          KC_Y   , KC_P   , KC_DOT , KC_COMM, KC_SCLN, KC_GRV ,
+      KC_LSFT, KC_S   , KC_N   , KC_T   , KC_H   , KC_D   ,                                          KC_I   , KC_U   , KC_E   , KC_O   , KC_A   , KC_RSFT,
+      KC_LCTL, KC_Z   , KC_V   , KC_W   , KC_M   , KC_B   , _______, _______,      _______, _______, KC_X   , KC_K   , KC_J   , KC_Q   , KC_QUOT, KC_RCTL,
                                  _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______
     ),
 
-    [GDM] = LAYOUT(
+    [GDN] = LAYOUT(
       KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_9   ,                                          _______, KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_GRV ,
       KC_LSFT, KC_5   , KC_6   , KC_7   , KC_8   , KC_0   ,                                          _______, KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_RSFT,
-      KC_LCTL, _______, _______, KC_F6  , KC_F5  , KC_F9  , _______, _______,      _______, _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_RCTL,
+      KC_LCTL, KC_F13 , KC_F14 , KC_F15 , KC_F16 , KC_F17 , _______, _______,      _______, _______, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_RCTL,
                                  _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______
     ),
 
